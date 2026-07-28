e00277c22893093063d21d49cab2e16fc0fd399a
projectsharrisons-lgtm <projectsharrisons@gmail.com>
2026-07-28 13:08:51 +0300

    forms.py

--- app/blueprints/admin/forms.py
+++ app/blueprints/admin/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+from app.models.academic import Department, Course, Unit
 
 class DepartmentForm(FlaskForm):
     name = StringField('Department Name', validators=[DataRequired(), Length(max=150)])
